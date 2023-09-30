4fd9132a7ba80d0ea6f6c9b134b3e879d38960d4
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-09-30 09:25:24 +0600

    Created MainRoutes and MainLayout components.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import MainRoutes from './Routes/MainRoutes';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={MainRoutes}></RouterProvider>
+  </React.StrictMode>
+);
